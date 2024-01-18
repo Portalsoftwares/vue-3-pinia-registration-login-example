213c7142777f70bee33261c4e6640bb1680534d0
rafael sanchez port <rafysanchez@hotmail.com>
2024-01-18 00:12:31 -0300

    ajustes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { useAuthStore } from '@/stores';
+import { useAuthStore } from "@/stores";
 
 const authStore = useAuthStore();
 </script>
-
+<!-- 
 <template>
     <nav v-show="authStore.user" class="navbar navbar-expand navbar-dark bg-dark">
         <div class="navbar-nav">
@@ -12,4 +12,41 @@ const authStore = useAuthStore();
             <button @click="authStore.logout()" class="btn btn-link nav-item nav-link">Logout</button>
         </div>
     </nav>
+</template> -->
+
+<template>
+  <nav v-show="authStore.user" class="navbar navbar-expand navbar-dark bg-dark">
+    <div class="navbar-nav">
+      <router-link to="/" class="nav-item nav-link order-1">Home</router-link>
+    </div>
+
+    <div class="navbar-nav ml-auto">
+      <span class="ispan"> {{ authStore.user.firstName }} </span>
+      <router-link to="/users" class="nav-item nav-link">Users</router-link>
+
+      <button
+        @click="authStore.logout()"
+        class="btn btn-link nav-item nav-link text-white logout-btn"
+      >
+        Logout
+      </button>
+    </div>
+  </nav>
 </template>
+
+<style>
+.logout-btn:hover {
+  font-weight: bold;
+  font-size: 1.5em; /* aumenta o tamanho em 50% */
+}
+.nav-item:hover {
+  text-align: center;
+}
+.ispan{
+  font-size: 1.5em;
+  font-weight: bold;
+  color: white;
+  margin-right: 10px;
+
+}
+</style>
